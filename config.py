fa09246698d20abb201cadc047812524689b8a8d
TaeJoon Kim <taejoonkim0119@gmail.com>
2025-04-19 14:56:40 -0400

    test gitignore

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-CLIENT_ID = ''
-CLIENT_SECRET = ''
-USER_AGENT = 'script:reddit_analyzer:v1.0 (by u/your_user_name'
+CLIENT_ID = 'lYLhVEN-w5LzsvRtnzQP8g'
+CLIENT_SECRET = 'NVoYDdVdZrHuO5ixKRtpWQfmwkr6aA'
+USER_AGENT = 'script:reddit_analyzer:v1.0 (by u/LowRegular6891)'
